8ec9357cb3a7e032b11b684aced1e8425342bfaa
Carlos <carlos_charlie95@hotmail.com>
2020-01-14 19:49:50 +0100

    Added update test for the database

--- tests/integration-db.test.js
+++ tests/integration-db.test.js
@@ -10,12 +10,6 @@ describe("Reviews DB connection", () => {
         return db();
     });
 
-    beforeEach((done) => {
-        Review.deleteMany({}, (err) => {
-            done();
-        })
-    });
-
     it ("Writes a Review in the DB", (done) => {
         const review = new Review({
             "impressions": {
@@ -30,7 +24,7 @@ describe("Reviews DB connection", () => {
             "created": "2019-12-10T19:09:36.884Z"
           });
 
-          review.save((err, review) => {
+          review.save((err) => {
               expect(err).toBeNull();
               Review.find({}, (err, reviews) => {
                 expect(reviews).toBeArrayOfSize(1);
@@ -39,11 +33,66 @@ describe("Reviews DB connection", () => {
           });
     });
 
+    
+    it ("Updates that Review in the DB", (done) => {
+          Review.updateOne({imdbId: 'tt0903747'},{imdbId:'tt090374'},(err) => {
+              expect(err).toBeNull();
+              Review.find({}, (err, reviews) => {
+                expect(reviews[0].imdbId).toBe('tt090374');
+                done();
+              });
+          });
+    });
+
     afterAll((done) => {
-        mongoose.connection.db.dropDatabase(() => {
-            mongoose.connection.close(done);
-        })
+        Review.deleteMany({}, (err) => {
+            mongoose.connection.db.dropDatabase(() => {
+                mongoose.connection.close(done);
+            });
+        });
     });
 
 
+
+
 });
+
+/*describe("The DB updates the reviews",()=>{
+
+        
+        beforeEach((done) => {
+            Review.deleteMany({}, (err) => {
+                done();
+            })
+        });
+
+        it ("Updates a Review in the DB",async (done) => {
+
+            const review = new Review({
+                "impressions": {
+                "likes": 10,
+                "dislikes": 0,
+                "spam": 0
+                },
+                "imdbId": "tt0903747",
+                "rating": 4,
+                "title": "Buena película",
+                "user": "example",
+                "created": "2019-12-10T19:09:36.884Z"
+            });
+
+            review.updateOne((err, review) => {
+                    expect(err).toBeNull();
+                    expect(reviews).toBeArrayOfSize(1);
+                    done();
+                });
+            });
+
+
+        afterAll((done) => {
+            mongoose.connection.db.dropDatabase(() => {
+                mongoose.connection.close(done);
+            })
+        });
+
+});*/
